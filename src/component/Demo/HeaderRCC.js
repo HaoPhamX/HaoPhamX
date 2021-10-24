197dd176e0b20ea35d67aba56d5c98f3707c6f41
Pham Xuan Hao <phamxuanhao1297@gmail.com>
2021-10-24 22:59:40 +0700

    upload_bai_tap_layout_slider2

---
+++ src/component/Demo/HeaderRCC.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component { //React class component
+    render() {
+        return (
+            <div>
+                <h1>ĐẠI CA HÀO RCC</h1>
+            </div>
+        )
+    }
+}
